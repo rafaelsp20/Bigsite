60337d763e9e4175068fc828987b583354943ef9
Rafael Ferreira <rafaelsp20@hotmail.com>
2024-10-24 20:27:39 -0300

    Segundo Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,11 @@ function Header() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/teste">Home</Link></li>
         <li><Link to="/servico">Serviço</Link></li>
         <li><Link to="/galeria">Galeria de Fotos</Link></li>
         <li><Link to="/contato1">Contato</Link></li>
-        <li><Link to="/teste">Teste</Link></li>
-
+        
       </ul>
     </nav>
   );
