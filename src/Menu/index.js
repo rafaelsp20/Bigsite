f65d9ac37331f04e507d1c988baa4726eebc2f8f
Rafael Ferreira <rafaelsp20@hotmail.com>
2024-11-11 12:56:08 -0300

    Correções Arquivo Menu

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./styles.css";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { ReactComponent as Logo } from "./logo.svg";
-
+/*NavDropdown*/
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function AppMenu() {
